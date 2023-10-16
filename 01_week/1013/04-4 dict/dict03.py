690dc8f5983b74e5f0ef3a033f084bfb8e19b332
duck82father <legnaevol84@gmail.com>
2023-10-16 17:40:23 +0900

    2023-10-16

---
+++ 01_week/1013/04-4 dict/dict03.py
@@ -0,0 +1,14 @@
+# 딕셔너리 선언
+dictionary = {
+    "name":"7D 건조 망고",
+    "type":"당절임"
+    }
+
+# 요소 추가 전의 내용 출력
+print("요소 제거 이전 :", dictionary)
+
+# 딕셔너리 요소 제거
+del dictionary["name"]
+
+# 출력
+print("요소 제거 이후 :",dictionary)
